5aee6da4f7db00ae14553ed07ba659eb63a4d25d
akimon658 <akimo_greninja@yahoo.co.jp>
2022-05-15 16:35:34 +0900

    Add `<C-n>`/<C-p>` mapping for nvim-cmp

--- home-manager/nvim/lua/config.lua
+++ home-manager/nvim/lua/config.lua
@@ -3,15 +3,40 @@ capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local cmp = require('cmp')
+local luasnip = require('luasnip')
+local function has_words_before()
+  local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line-1, line, true)[1]:sub(col, col):match('%s') == nil
+end
 cmp.setup {
   mapping = {
+    ['<C-n>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ['<C-p>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
     ['<Tab>'] = cmp.mapping.confirm {
       select = true
     }
   },
   snippet = {
     expand = function(args)
-      require('luasnip').lsp_expand(args.body)
+      luasnip.lsp_expand(args.body)
     end
   },
   sources = {
