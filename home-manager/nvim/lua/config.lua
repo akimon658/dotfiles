4df5573fc4484c5ca0ed6c3f66f4d93654e7298b
akimon658 <akimo_greninja@yahoo.co.jp>
2022-08-26 06:02:45 +0900

    Add bashls

--- home-manager/nvim/lua/config.lua
+++ home-manager/nvim/lua/config.lua
@@ -50,6 +50,7 @@ UseDeno = vim.fn.filereadable(vim.fn.getcwd() .. '/deno.json') == 1
 local tsserver = UseDeno and 'denols' or 'tsserver'
 
 local servers = {
+  'bashls',
   'clangd',
   'cssls',
   'gopls',
