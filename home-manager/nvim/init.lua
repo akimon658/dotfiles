219f314328f2b997cd9caea8fde9f94717a15f55
Akimon658 <akimo_greninja@yahoo.co.jp>
2023-03-02 17:39:41 +0900

    Enable `use_libuv_file_watcher`

--- home-manager/nvim/init.lua
+++ home-manager/nvim/init.lua
@@ -221,7 +221,8 @@ local plugins = {
         close_if_last_window = true,
         filesystem = {
           filtered_items = { visible = true },
-          follow_current_file = true
+          follow_current_file = true,
+          use_libuv_file_watcher = true
         },
         view = { relativenumber = true },
         window = { width = 30 }
