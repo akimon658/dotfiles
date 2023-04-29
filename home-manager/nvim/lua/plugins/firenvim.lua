a6100ffcd36420fff2e2da47dc9b33fa105b4ba8
Akimon658 <akimo_greninja@yahoo.co.jp>
2023-04-30 08:49:51 +0900

    Add Firenvim setting for Go Playground

--- home-manager/nvim/lua/plugins/firenvim.lua
+++ home-manager/nvim/lua/plugins/firenvim.lua
@@ -10,18 +10,37 @@ local firenvim = {
   end,
   cond = not not vim.g.started_by_firenvim,
   config = function()
-    vim.api.nvim_create_autocmd('BufEnter', {
-      pattern = 'github.com_*.txt',
-      callback = function()
-        vim.opt_local.filetype = 'markdown'
-      end
-    })
+    ---@class filetypeConfig
+    ---@field pattern string
+    ---@field ft string
+
+    ---@type filetypeConfig[]
+    local configs = {
+      {
+        pattern = 'github.com_*',
+        ft = 'markdown'
+      },
+      {
+        pattern = 'go.dev_*',
+        ft = 'go'
+      }
+    }
+
+    for i in ipairs(configs) do
+      vim.api.nvim_create_autocmd('BufEnter', {
+        pattern = string.format('%s.txt', configs[i].pattern),
+        callback = function()
+          vim.opt_local.filetype = configs[i].ft
+        end
+      })
+    end
 
     vim.g.firenvim_config = {
       localSettings = {
         [ [[.*]] ] = { takeover = 'never' },
         [ [[github\.com]] ] = { takeover = 'always' },
-        [ [[github\.com.*blob]] ] = { takeover = 'never' }
+        [ [[github\.com.*blob]] ] = { takeover = 'never' },
+        [ [[go\.dev]] ] = { takeover = 'always' }
       }
     }
 
