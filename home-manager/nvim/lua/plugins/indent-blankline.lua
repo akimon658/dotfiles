1e083efcac91c39b9a847002a824928b71990949
akimon658 <akimo_greninja@yahoo.co.jp>
2024-03-06 16:01:46 +0900

    Update indent-blankline config

--- home-manager/nvim/lua/plugins/indent-blankline.lua
+++ home-manager/nvim/lua/plugins/indent-blankline.lua
@@ -5,18 +5,16 @@ local indent_blankline = {
   'lukas-reineke/indent-blankline.nvim',
   config = function()
     local link_to_non_text = { link = 'NonText' }
-    vim.api.nvim_set_hl(0, 'IndentBlanklineChar', link_to_non_text)
-    vim.api.nvim_set_hl(0, 'IndentBlanklineContextChar', { fg = '#dcdcaa' })
-    vim.api.nvim_set_hl(0, 'IndentBlanklineSpaceChar', link_to_non_text)
-    require('indent_blankline').setup({
-      show_current_context = true
-    })
+    vim.api.nvim_set_hl(0, 'IblIndent', link_to_non_text)
+    vim.api.nvim_set_hl(0, 'IblScope', { fg = '#dcdcaa' })
+    vim.api.nvim_set_hl(0, 'IblWhitespace', link_to_non_text)
   end,
   dependencies = { require('plugins.treesitter') },
   event = {
     event.buf_new_file,
     event.buf_read_pre
-  }
+  },
+  main = 'ibl',
 }
 
 return indent_blankline
