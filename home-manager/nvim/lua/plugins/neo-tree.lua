495923b3f24f2c31484a8492cb509703b623137d
akimon658 <akimo_greninja@yahoo.co.jp>
2024-03-07 11:04:34 +0900

    Update neo-tree setting

--- home-manager/nvim/lua/plugins/neo-tree.lua
+++ home-manager/nvim/lua/plugins/neo-tree.lua
@@ -8,7 +8,7 @@ local neo_tree = {
       close_if_last_window = true,
       filesystem = {
         filtered_items = { visible = true },
-        follow_current_file = true,
+        follow_current_file = { enabled = true },
         use_libuv_file_watcher = true
       },
       view = { relativenumber = true },
