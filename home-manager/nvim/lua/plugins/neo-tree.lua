38a304e2b531514bc61f7e140e546d5413805cd0
Takumi Akimoto <akimo_greninja@yahoo.co.jp>
2024-03-31 18:49:09 +0900

    Don't show icon for deleted files on neo-tree

--- home-manager/nvim/lua/plugins/neo-tree.lua
+++ home-manager/nvim/lua/plugins/neo-tree.lua
@@ -4,21 +4,28 @@ local neo_tree = {
   config = function()
     vim.g.neo_tree_remove_legacy_commands = 1
 
-    require('neo-tree').setup({
+    require('neo-tree').setup {
       close_if_last_window = true,
+      default_component_configs = {
+        git_status = {
+          symbols = {
+            deleted = ""
+          },
+        },
+      },
       filesystem = {
         filtered_items = { visible = true },
         follow_current_file = { enabled = true },
         use_libuv_file_watcher = true
       },
       view = { relativenumber = true },
-      window = { width = 30 }
-    })
+      window = { width = 30 },
+    }
   end,
   dependencies = {
     require('plugins.devicons'),
     require('plugins.nui'),
-    require('plugins.plenary')
+    require('plugins.plenary'),
   },
   keys = { {
     '<C-e>',
