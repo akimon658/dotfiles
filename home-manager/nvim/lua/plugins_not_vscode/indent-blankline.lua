a3cb896e1bab2a8a51c9c0d1510187832333af38
Takumi Akimoto <akimoto.t.aa@m.titech.ac.jp>
2024-05-02 22:51:00 +0900

    Properly configure indent-blankline

--- home-manager/nvim/lua/plugins_not_vscode/indent-blankline.lua
+++ home-manager/nvim/lua/plugins_not_vscode/indent-blankline.lua
@@ -2,19 +2,23 @@ local event = require "vim_event"
 
 ---@type LazyPluginSpec
 local indent_blankline = {
-	"lukas-reineke/indent-blankline.nvim",
-	config = function()
-		local link_to_non_text = { link = "NonText" }
-		vim.api.nvim_set_hl(0, "IblIndent", link_to_non_text)
-		vim.api.nvim_set_hl(0, "IblScope", { fg = "#dcdcaa" })
-		vim.api.nvim_set_hl(0, "IblWhitespace", link_to_non_text)
-	end,
-	dependencies = { require "plugins_not_vscode.treesitter" },
-	event = {
-		event.buf_new_file,
-		event.buf_read_pre,
-	},
-	main = "ibl",
+  "lukas-reineke/indent-blankline.nvim",
+  config = function()
+    local link_to_non_text = { link = "NonText" }
+    vim.api.nvim_set_hl(0, "IblIndent", link_to_non_text)
+    vim.api.nvim_set_hl(0, "IblScope", { fg = "#dcdcaa" })
+    vim.api.nvim_set_hl(0, "IblWhitespace", link_to_non_text)
+    require "ibl".setup {
+      indent = {
+        char = "│",
+      },
+    }
+  end,
+  dependencies = { require "plugins_not_vscode.treesitter" },
+  event = {
+    event.buf_new_file,
+    event.buf_read_pre,
+  },
 }
 
 return indent_blankline
