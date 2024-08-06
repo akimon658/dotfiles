f7adf4bbe7f8753ad5e8170ef6f687f2e7b3648b
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-08-06 12:50:38 +0900

    Fix tab indentation to spaces

--- home-manager/nvim/lua/plugins_not_vscode/cmp.lua
+++ home-manager/nvim/lua/plugins_not_vscode/cmp.lua
@@ -1,87 +1,87 @@
 ---@type LazyPluginSpec
 local cmp = {
-	"hrsh7th/nvim-cmp",
-	config = function()
-		local cmp = require "cmp"
-		local luasnip = require "luasnip"
-		---@return boolean
-		local function has_words_before()
-			local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
-			return col ~= 0 and
-			vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-		end
-		local bordered_window = cmp.config.window.bordered()
+  "hrsh7th/nvim-cmp",
+  config = function()
+    local cmp = require "cmp"
+    local luasnip = require "luasnip"
+    ---@return boolean
+    local function has_words_before()
+      local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and
+          vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+    end
+    local bordered_window = cmp.config.window.bordered()
 
-		cmp.setup {
-			formatting = { format = require "lspkind".cmp_format() },
-			mapping = {
-				["<C-n>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
-					elseif has_words_before() then
-						cmp.complete()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<C-p>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<Tab>"] = cmp.mapping.confirm {
-					select = true,
-				},
-			},
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			sources = {
-				{ name = "luasnip" },
-				{ name = "nvim_lsp" },
-				{ name = "nvim_lua" },
-				{ name = "path" },
-			},
-			window = {
-				completion = bordered_window,
-				documentation = bordered_window,
-			},
-		}
+    cmp.setup {
+      formatting = { format = require "lspkind".cmp_format() },
+      mapping = {
+        ["<C-n>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<C-p>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<Tab>"] = cmp.mapping.confirm {
+          select = true,
+        },
+      },
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      sources = {
+        { name = "luasnip" },
+        { name = "nvim_lsp" },
+        { name = "nvim_lua" },
+        { name = "path" },
+      },
+      window = {
+        completion = bordered_window,
+        documentation = bordered_window,
+      },
+    }
 
-		cmp.setup.cmdline(":", {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = cmp.config.sources {
-				{ name = "cmdline" },
-				{ name = "path" },
-			},
-		})
-	end,
-	dependencies = {
-		"hrsh7th/cmp-cmdline",
-		"hrsh7th/cmp-nvim-lua",
-		{
-			"hrsh7th/cmp-nvim-lsp",
-			dependencies = { require "plugins_not_vscode.lspconfig" },
-		},
-		"hrsh7th/cmp-path",
-		"onsails/lspkind.nvim",
-		{
-			"saadparwaiz1/cmp_luasnip",
-			dependencies = { "L3MON4D3/LuaSnip" },
-		},
-	},
-	event = {
-		"CmdlineEnter",
-		require "vim_event".insert_enter,
-	},
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources {
+        { name = "cmdline" },
+        { name = "path" },
+      },
+    })
+  end,
+  dependencies = {
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-nvim-lua",
+    {
+      "hrsh7th/cmp-nvim-lsp",
+      dependencies = { require "plugins_not_vscode.lspconfig" },
+    },
+    "hrsh7th/cmp-path",
+    "onsails/lspkind.nvim",
+    {
+      "saadparwaiz1/cmp_luasnip",
+      dependencies = { "L3MON4D3/LuaSnip" },
+    },
+  },
+  event = {
+    "CmdlineEnter",
+    require "vim_event".insert_enter,
+  },
 }
 
 return cmp
--- home-manager/nvim/lua/plugins_not_vscode/neo-tree.lua
+++ home-manager/nvim/lua/plugins_not_vscode/neo-tree.lua
@@ -1,38 +1,38 @@
 ---@type LazyPluginSpec
 local neo_tree = {
-	"nvim-neo-tree/neo-tree.nvim",
-	config = function()
-		vim.g.neo_tree_remove_legacy_commands = 1
+  "nvim-neo-tree/neo-tree.nvim",
+  config = function()
+    vim.g.neo_tree_remove_legacy_commands = 1
 
-		require "neo-tree".setup {
-			close_if_last_window = true,
-			default_component_configs = {
-				git_status = {
-					symbols = {
-						deleted = "",
-					},
-				},
-			},
-			filesystem = {
-				filtered_items = { visible = true },
-				follow_current_file = { enabled = true },
-				use_libuv_file_watcher = true,
-			},
-			view = { relativenumber = true },
-			window = { width = 30 },
-		}
-	end,
-	dependencies = {
-		require "plugins_not_vscode.devicons",
-		require "plugins_not_vscode.nui",
-		require "plugins.plenary",
-	},
-	keys = { {
-		"<C-e>",
-		function()
-			require "neo-tree.command"._command "toggle"
-		end,
-	} },
+    require "neo-tree".setup {
+      close_if_last_window = true,
+      default_component_configs = {
+        git_status = {
+          symbols = {
+            deleted = "",
+          },
+        },
+      },
+      filesystem = {
+        filtered_items = { visible = true },
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+      },
+      view = { relativenumber = true },
+      window = { width = 30 },
+    }
+  end,
+  dependencies = {
+    require "plugins_not_vscode.devicons",
+    require "plugins_not_vscode.nui",
+    require "plugins.plenary",
+  },
+  keys = { {
+    "<C-e>",
+    function()
+      require "neo-tree.command"._command "toggle"
+    end,
+  } },
 }
 
 return neo_tree
