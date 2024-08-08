d12cc8deb46971acf2a1b7b593bb914ec6ad833c
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-08-08 16:14:56 +0900

    Add a setting for `goimports`

--- home-manager/nvim/lua/plugins_not_vscode/lspconfig.lua
+++ home-manager/nvim/lua/plugins_not_vscode/lspconfig.lua
@@ -22,6 +22,28 @@ local lspconfig = {
           pattern = any_pattern,
         },
       },
+      -- https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-imports
+      {
+        event = event.buf_write_pre,
+        config = {
+          group = vim.api.nvim_create_augroup("goimports", {}),
+          callback = function()
+            local params = vim.lsp.util.make_range_params()
+            params.context = { only = { "source.organizeImports" } }
+            local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+
+            for cid, res in pairs(result or {}) do
+              for _, r in pairs(res.result or {}) do
+                if r.edit then
+                  local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encodint or "utf-16"
+                  vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                end
+              end
+            end
+          end,
+          pattern = "*.go",
+        },
+      },
       {
         event = "CursorHold",
         config = {
