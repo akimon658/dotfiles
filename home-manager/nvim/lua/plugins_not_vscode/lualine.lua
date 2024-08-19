323f8fe81b4eb73e1d0f94e9d4dcc46d8beefce7
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-08-19 16:37:21 +0900

    Install copilot-lualine

--- home-manager/nvim/lua/plugins_not_vscode/lualine.lua
+++ home-manager/nvim/lua/plugins_not_vscode/lualine.lua
@@ -4,10 +4,25 @@ local lualine = {
   dependencies = {
     require "plugins_not_vscode.devicons",
     require "plugins_not_vscode.vscode",
+    {
+      "AndreM222/copilot-lualine",
+      dependencies = { require "plugins_not_vscode.copilot" },
+    },
   },
   opts = {
     options = {
       globalstatus = true,
+    },
+    sections = {
+      lualine_x = {
+        {
+          "copilot",
+          show_colors = true
+        },
+        "encoding",
+        "fileformat",
+        "filetype",
+      },
     },
   },
 }
