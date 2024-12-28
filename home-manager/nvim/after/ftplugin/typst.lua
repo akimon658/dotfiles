f2829b56ca0f85b2c997d075d49b36caafa0ecea
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-12-28 17:15:25 +0900

    Replace periods and commas on save

--- home-manager/nvim/after/ftplugin/typst.lua
+++ home-manager/nvim/after/ftplugin/typst.lua
@@ -3,3 +3,17 @@ require "ftbase.text"
 require "nvim-autopairs".add_rule(
   require "nvim-autopairs.rule" ("$", "$", "typst")
 )
+
+local util = require "vim_util"
+
+util.create_autocmd {
+  event = require "vim_event".buf_write_pre,
+  config = {
+    group = util.create_augroup "typst_fmt",
+    callback = function()
+      vim.cmd ":silent! %s/。/．/g"
+      vim.cmd ":silent! %s/、/，/g"
+    end,
+    pattern = "*.typ",
+  },
+}
