76f71c10b87572e9e76efe5d1536f28f711f3836
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-06-14 09:31:25 +0900

    Make tab line and status line transparent

--- home-manager/nvim/lua/plugins/vscode.lua
+++ home-manager/nvim/lua/plugins/vscode.lua
@@ -6,6 +6,8 @@ local vscode_plugin = {
     local is_wezterm = os.getenv "TERM_PROGRAM" == "WezTerm"
     vscode.setup { transparent = is_wezterm }
     vscode.load()
+    vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
   end,
 }
 
