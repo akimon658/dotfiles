063d8a38ac64c793136ef54e6d8d9e3b56972474
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-09-23 14:55:10 +0900

    Enable inlay hints for denols

--- home-manager/nvim/after/lsp/denols.lua
+++ home-manager/nvim/after/lsp/denols.lua
@@ -1,4 +1,16 @@
 return {
   root_markers = { "deno.json", "deno.jsonc" },
+  settings = {
+    deno = {
+      inlayHints = {
+        parameterNames = { enabled = "all", suppressWhenArgumentMatchesName = true },
+        parameterTypes = { enabled = true },
+        variableTypes = { enabled = true, suppressWhenTypeMatchesName = true },
+        propertyDeclarationTypes = { enabled = true },
+        functionLikeReturnTypes = { enable = true },
+        enumMemberValues = { enabled = true },
+      },
+    },
+  },
   workspace_required = true,
 }
