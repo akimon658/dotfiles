a0b623c5d085a522b5e191b595c959f721782913
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-12-19 05:42:18 +0900

    Enable Touch ID auth for sudo

--- darwin/default.nix
+++ darwin/default.nix
@@ -2,6 +2,7 @@
   nix.settings.experimental-features = "nix-command flakes";
   nixpkgs.hostPlatform = "aarch64-darwin";
   programs.fish.enable = true;
+  security.pam.services.sudo_local.touchIdAuth = true;
   system = {
     configurationRevision = self.rev or self.dirtyRev or null;
     stateVersion = 6;
