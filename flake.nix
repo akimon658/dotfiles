293291a29ef447ad009e4683c30caac243be8f2c
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-12-19 03:35:42 +0900

    Set nix-darwin up

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A flake to provision @akimon658's environment";
+
+  inputs = {
+    nix-darwin = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  {
+    darwinConfigurations."Nozomi" = nix-darwin.lib.darwinSystem {
+      modules = [
+        {
+          nix.settings.experimental-features = "nix-command flakes";
+          nixpkgs.hostPlatform = "aarch64-darwin";
+          programs.fish.enable = true;
+          system = {
+            configurationRevision = self.rev or self.dirtyRev or null;
+            stateVersion = 6;
+          };
+        }
+      ];
+    };
+  };
+}
