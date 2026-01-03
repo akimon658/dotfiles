46d16c13200417277bd69c98a852e6323c438304
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2026-01-03 23:33:14 +0900

    Migrate from WezTerm to Ghostty

--- home-manager/nvim/lua/plugins/vscode.lua
+++ home-manager/nvim/lua/plugins/vscode.lua
@@ -3,8 +3,8 @@ local vscode_plugin = {
   "Mofiqul/vscode.nvim",
   config = function()
     local vscode = require "vscode"
-    local is_wezterm = os.getenv "TERM_PROGRAM" == "WezTerm"
-    vscode.setup { transparent = is_wezterm }
+    local is_ghostty = os.getenv "TERM_PROGRAM" == "ghostty"
+    vscode.setup { transparent = is_ghostty }
     vscode.load()
     vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
     vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
