c5965448c1e0c402ff8ed41e17997a2ad6f7cf9e
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2026-02-09 10:35:28 +0900

    Configure Markdown heading style

--- home-manager/nvim/lua/plugins/render-markdown.lua
+++ home-manager/nvim/lua/plugins/render-markdown.lua
@@ -2,4 +2,21 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
   ft = { "codecompanion", "markdown" },
+  ---@type render.md.UserConfig
+  opts = {
+    heading = {
+      backgrounds = {
+        "None",
+        "None",
+        "None",
+        "None",
+        "None",
+        "None",
+      },
+      icons = {
+        "# ", "## ", "### ", "#### ", "##### ", "###### ",
+      },
+      sign = false,
+    },
+  },
 }
