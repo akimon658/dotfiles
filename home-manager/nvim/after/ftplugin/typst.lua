e6232ad5a2605d316d1bde00dfedeecdd40634af
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2026-02-09 09:34:27 +0900

    Don't replace punctuations on save

--- home-manager/nvim/after/ftplugin/typst.lua
+++ home-manager/nvim/after/ftplugin/typst.lua
@@ -3,17 +3,3 @@ vim.opt_local.wrap = true
 require "nvim-autopairs".add_rule(
   require "nvim-autopairs.rule" ("$", "$", "typst"):with_move(function() return true end)
 )
-
-local util = require "vim_util"
-
-util.create_autocmd {
-  event = require "vim_event".buf_write_pre,
-  config = {
-    group = util.create_augroup "typst_fmt",
-    callback = function()
-      vim.cmd ":silent! %s/。/．/g"
-      vim.cmd ":silent! %s/、/，/g"
-    end,
-    pattern = "*.typ",
-  },
-}
