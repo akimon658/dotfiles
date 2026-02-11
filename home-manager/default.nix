ddbcd5ba16cccfc932cdc91c1b763bf96a0a6885
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2026-02-11 14:25:54 +0900

    Manage git config

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -37,18 +37,39 @@ in {
     stateVersion = "25.11";
     username = username;
   };
-  programs.ghostty = {
-    enable = true;
-    package = pkgs.brewCasks.ghostty;
-    settings = {
-      background-opacity = 0.9;
-      command = "${pkgs.unstable.fish}/bin/fish";
-      font-family = [
-        "Roboto Mono"
-        "Noto Sans JP"
-      ];
-      macos-titlebar-style = "tabs";
-      theme = "dark:One Double Dark, light:One Double Light";
+  programs = {
+    ghostty = {
+      enable = true;
+      package = pkgs.brewCasks.ghostty;
+      settings = {
+        background-opacity = 0.9;
+        command = "${pkgs.unstable.fish}/bin/fish";
+        font-family = [
+          "Roboto Mono"
+          "Noto Sans JP"
+        ];
+        macos-titlebar-style = "tabs";
+        theme = "dark:One Double Dark, light:One Double Light";
+      };
+    };
+    git = {
+      enable = true;
+      settings = {
+        core.editor = "nvim";
+        ghq.root = "~/Google Drive/マイドライブ/ghq";
+        help.autocorrect = "50";
+        user = {
+          name = "Takumi Akimoto";
+          email = "81888693+akimon658@users.noreply.github.com";
+        };
+      };
+      signing = {
+        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINJUJW10aa7BhexEnWNWgRdzMHMh/rlODWF0H8SSBfmm";
+        format = "ssh";
+        signByDefault = true;
+        signer = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+      };
+      ignores = [ ".DS_Store" ];
     };
   };
   xdg.configFile = {
