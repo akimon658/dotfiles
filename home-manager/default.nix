1bd4903bd4d1b3f90178119b13deff422f459924
Takumi Akimoto <noreply@akimo.dev>
2026-05-04 18:43:05 +0900

    Add starship

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: let
+{ pkgs, lib, ... }: let
   username = "akimon658";
 in {
   imports = [ ./nvim.nix ];
@@ -103,6 +103,95 @@ in {
         ".DS_Store"
         ".envrc"
       ];
+    };
+    starship = {
+      enable = true;
+      settings = {
+        format = lib.strings.concatStrings [
+            "$os"
+            "[](bg:#769ff0 fg:#a3aed2)"
+            "$nix_shell"
+            "$directory"
+            "[](fg:#769ff0 bg:#394260)"
+            "$git_branch"
+            "$git_status"
+            "[](fg:#394260 bg:#212736)"
+            "$nodejs"
+            "$rust"
+            "$golang"
+            "$php"
+            "$python"
+            "[](fg:#212736 bg:#1d2230)"
+            "$time"
+            "[ ](fg:#1d2230)"
+          ] + "\n$character";
+        directory = {
+          style = "fg:#e3e5e5 bg:#769ff0";
+          format = "[ $path ]($style)";
+          truncation_length = 3;
+          truncation_symbol = "…/";
+          substitutions = {
+            Documents = "󰈙 ";
+            Downloads = " ";
+            Music = " ";
+            Pictures = " ";
+          };
+        };
+        git_branch = {
+          symbol = "";
+          style = "bg:#394260";
+          format = "[[ $symbol $branch ](fg:#769ff0 bg:#394260)]($style)";
+        };
+        git_status = {
+          style = "bg:#394260";
+          format = "[[($all_status$ahead_behind )](fg:#769ff0 bg:#394260)]($style)";
+        };
+        nix_shell = {
+          symbol = "";
+          style = "fg:769ff0 bg:#769ff0";
+          format = "[[ $symbol ](fg:#e3e5e5 bg:#769ff0)]($style)";
+        };
+        nodejs = {
+          symbol = "";
+          style = "bg:#212736";
+          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+        };
+        rust = {
+          symbol = "";
+          style = "bg:#212736";
+          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+        };
+        golang = {
+          symbol = "";
+          style = "bg:#212736";
+          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+        };
+        php = {
+          symbol = "";
+          style = "bg:#212736";
+          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+        };
+        python = {
+          symbol = "";
+          style = "bg:#212736";
+          format = "[[ $symbol ($version)( \\($virtualenv\\)) ](fg:#769ff0 bg:#212736)]($style)";
+        };
+        time = {
+          disabled = false;
+          time_format = "%R"; # Hour:Minute Format
+          style = "bg:#1d2230";
+          format = "[[  $time ](fg:#a0a9cb bg:#1d2230)]($style)";
+        };
+        os = {
+          disabled = false;
+          format = "[ $symbol ](bg:#a3aed2 fg:#090c0c)";
+          symbols = {
+            Arch = "󰣇";
+            Macos = "";
+            Ubuntu = "";
+          };
+        };
+      };
     };
   };
 }
